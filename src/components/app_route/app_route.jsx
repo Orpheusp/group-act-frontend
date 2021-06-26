11eed9443f38361624ae7302230b8c688df2af21
Orpheus Peng <stannolite@gmail.com>
2021-06-25 20:04:22 -0700

    Define a Route component specific to Group Act

---
+++ src/components/app_route/app_route.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { useAuth } from '../services/auth_service/auth_service';
+import { Route, Redirect } from 'react-router-dom';
+
+export function AppRoute({ children, ...rest }) {
+  const auth = useAuth();
+
+  const render = ({ location }) => {
+    switch (location) {
+      // When destination is the user or the group front page.
+      case '/user':
+      case '/group':
+        if (auth.user) {
+          return children;
+        } else {
+          return redirectTo(location, '/');
+        }
+      // When destination is the welcome page.
+      case '/':
+        if (auth.user) {
+          return redirectTo(location, '/user');
+        } else {
+          return children;
+        }
+      default:
+        return redirectTo(location, '/');
+    }
+  };
+
+  return <Route {...rest} render={render} />;
+}
+
+function redirectTo(from, to) {
+  const dest = {
+    pathname: to,
+    state: { from },
+  };
+  return <Redirect to={dest} />;
+}
