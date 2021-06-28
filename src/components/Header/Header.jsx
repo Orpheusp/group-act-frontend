e32e8f8609bb34b532f09da2e02fc892243d16d5
Orpheus Peng <stannolite@gmail.com>
2021-06-28 14:41:38 -0700

    Define Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './Header.css';
 
-export function Header() {
-  return (
-    <div>
-      <h1 className="header">GROUP ACT</h1>
-    </div>
-  );
+export function Header({ text }) {
+  return <div className="header">{text}</div>;
 }
