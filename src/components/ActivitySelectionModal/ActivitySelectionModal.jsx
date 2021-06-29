62e85b39a8af8a385b474b23702ad76e4dc4a245
Orpheus Peng <stannolite@gmail.com>
2021-06-28 19:42:58 -0700

    Define ActivitySelectionModal component

---
+++ src/components/ActivitySelectionModal/ActivitySelectionModal.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+
+import { ACTIVITY_LIST } from '../../ActivityList';
+
+import './ActivitySelectionModal.css';
+
+export function ActivitySelectionModal({
+  isOpen,
+  toggle,
+  activities,
+  onSelect,
+}) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      toggle={toggle}
+      centered={true}
+      scrollable={true}
+      className={'activity-selection-modal'}
+    >
+      <ModalHeader
+        toggle={toggle}
+        className={'activity-selection-modal--header'}
+      >
+        Activity Options
+      </ModalHeader>
+      <ModalBody className={'activity-selection-modal--body'}>
+        {activities.map((activity, index) => {
+          return (
+            <ActivityOption
+              key={index}
+              activity={activity}
+              onClick={() => onSelect(activity)}
+            />
+          );
+        })}
+      </ModalBody>
+    </Modal>
+  );
+}
+
+function ActivityOption({ onClick, activity, ...rest }) {
+  return (
+    <div
+      className={'activity-selection-modal--activity-option'}
+      onClick={onClick}
+      {...rest}
+    >
+      {ACTIVITY_LIST[activity]}
+    </div>
+  );
+}
