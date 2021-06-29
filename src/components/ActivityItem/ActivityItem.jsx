4d377df679a741b2892f31ed2e4186b6b201bfc7
Orpheus Peng <stannolite@gmail.com>
2021-06-28 20:31:56 -0700

    Define ActivityItem component

--- src/components/ActivityItem/ActivityItem.jsx
+++ src/components/ActivityItem/ActivityItem.jsx
@@ -1,40 +1,52 @@
 import React from 'react';
 
-import { AiFillCaretUp } from 'react-icons/ai';
-import { AiFillCaretDown } from 'react-icons/ai';
-import { AiFillDelete } from 'react-icons/ai';
+import { MdArrowUpward } from 'react-icons/md';
+import { MdArrowDownward } from 'react-icons/md';
+import { MdClose } from 'react-icons/md';
+
+import { ACTIVITY_LIST } from '../../ActivityList';
 
 import './ActivityItem.css';
 
-const ActivityItem = ({ acts, completeTodo, removeAct, updateTodo }) => {
-  // const [edit, setEdit] = useState({
-  //   id: null,
-  //   value: '',
-  // });
-
-  // const submitUpdate = (value) => {
-  //   updateTodo(edit.id, value);
-  //   setEdit({
-  //     id: null,
-  //     value: '',
-  //   });
-  // };
-
-  // if (edit.id) {
-  //   return <TodoForm edit={edit} onSubmit={submitUpdate} />;
-  // }
-
-  return acts.map((act, index) => (
-    <div className="todo-row" key={index}>
-      <div key={act.id}>{act.text}</div>
-      <div className="icons">
-        <AiFillCaretUp className="up" />
-        {/* onClick={() => removeTodo(todo.id)} */}
-        <AiFillCaretDown className="down" />
-        <AiFillDelete onClick={() => removeAct(act.id)} className="remove" />
+export function ActivityItem({
+  readonly = false,
+  activity,
+  select,
+  moveUp,
+  moveDown,
+  remove,
+  index,
+}) {
+  let parentClassName = 'activity-item';
+  let actions;
+  if (readonly) {
+    parentClassName = 'activity-item activity-item__readonly';
+  } else {
+    actions = (
+      <div className={'activity-item--actions'}>
+        <MdArrowUpward
+          onClick={moveUp}
+          className={'activity-item--action activity-item--move-up'}
+        />
+        <MdArrowDownward
+          onClick={moveDown}
+          className={'activity-item--action activity-item--move-down'}
+        />
+        <MdClose
+          onClick={remove}
+          className={'activity-item--action activity-item--remove'}
+        />
       </div>
-    </div>
-  ));
-};
+    );
+  }
 
-export default ActivityItem;
+  return (
+    <div className={parentClassName} key={activity}>
+      <div className={'activity-item--index'}>{index}</div>
+      <div className={'activity-item--label'} onClick={select}>
+        {ACTIVITY_LIST[activity]}
+      </div>
+      {actions}
+    </div>
+  );
+}
--- src/components/ActivityItem/ActivityItem.stories.jsx
+++ src/components/ActivityItem/ActivityItem.stories.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
 
-import { ActivityItem } from './ActivityItem.jsx';
+import { ActivityItem } from './ActivityItem';
 
 export default {
   title: 'ActivityItem',
   component: ActivityItem,
+  decorators: [
+    (Story) => (
+      <div className={'story-container'}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export const activityItem = () => {
-  return <ActivityItem />;
+  return (
+    <ActivityItem
+      activity={1}
+      select={() => {
+        console.log('select()');
+      }}
+      moveUp={() => {
+        console.log('moveUp()');
+      }}
+      moveDown={() => {
+        console.log('moveDown()');
+      }}
+      remove={() => {
+        console.log('remove()');
+      }}
+      index={12}
+    />
+  );
+};
+
+export const activityItemReadonly = () => {
+  return (
+    <ActivityItem
+      activity={1}
+      readonly={true}
+      select={() => {
+        console.log('select()');
+      }}
+      moveUp={() => {
+        console.log('moveUp()');
+      }}
+      moveDown={() => {
+        console.log('moveDown()');
+      }}
+      remove={() => {
+        console.log('remove()');
+      }}
+      index={12}
+    />
+  );
 };
