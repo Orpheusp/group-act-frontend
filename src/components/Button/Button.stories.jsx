07437b74f7699f3316a259e6339c613eb688ffee
Orpheus Peng <stannolite@gmail.com>
2021-06-29 15:10:03 -0700

    Modify disabled button style

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -18,10 +18,28 @@ export const buttonBlack = () => {
   return <Button text={'test'} buttonStyle={BUTTON_STYLE.BLACK} />;
 };
 
+export const buttonBlackDisabled = () => {
+  return (
+    <Button text={'test'} buttonStyle={BUTTON_STYLE.BLACK} disabled={true} />
+  );
+};
+
 export const buttonGreen = () => {
   return <Button text={'test'} buttonStyle={BUTTON_STYLE.GREEN} />;
 };
 
+export const buttonGreenDisabled = () => {
+  return (
+    <Button text={'test'} buttonStyle={BUTTON_STYLE.GREEN} disabled={true} />
+  );
+};
+
 export const buttonHollow = () => {
   return <Button text={'test'} buttonStyle={BUTTON_STYLE.HOLLOW} />;
+};
+
+export const buttonHollowDisabled = () => {
+  return (
+    <Button text={'test'} buttonStyle={BUTTON_STYLE.HOLLOW} disabled={true} />
+  );
 };
